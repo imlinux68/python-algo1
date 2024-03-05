7f8fb559313ea03976fde9e22ce0f521af274f46
imlinux68 <imlinux68@gmail.com>
2024-03-05 23:25:41 +0200

    improved check with composites

--- prim_num.py
+++ prim_num.py
@@ -4,14 +4,15 @@
 def prim_num():
     a = int(input("Enter a number: "))
     k = 0
+    composites = []
     for i in range(1, a+1):
         if a % i == 0:
             k += 1
-
+            composites.append(i)
     if k == 2:
         print("Number is primary!!!")
     else:
         print("Number isn't a primary")
-
+        print(composites)
 
 prim_num()
