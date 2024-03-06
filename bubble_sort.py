a151ae1b678ec4097d54d56f3be019f1e441dbd5
imlinux68 <imlinux68@gmail.com>
2024-03-06 13:10:33 +0200

    added buble sort

---
+++ bubble_sort.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python
+
+def bubble_sort():
+    nums = [2,3,7,5,1,4,9,7,8]
+    print(nums)
+
+    for i in range(len(nums)):
+        for j in range(len(nums)-i-1):
+            if nums[j] > nums[j+1]:
+                nums[j], nums[j+1] = nums[j+1], nums[j]
+
+    print(nums)
+
+bubble_sort()
