abc9c637378ffe87a3bac74b8d65faa0f6b64dc7
imlinux68 <imlinux68@gmail.com>
2024-03-08 21:02:05 +0200

    created square list

---
+++ list_sqr.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+def list_sqr():
+	li = [7, 8, 9, 25, 10, 2]
+	l1 = []
+	for x in li:
+		l1.append(x**2)
+	print(l1)
+
+
+
+
+
+if __name__ == "__main__":
+	list_sqr()
+
+
+
